26369a483846496f8c14f7a457a2a51fce156ade
hapo-chinhnc <68211429+hapo-chinhnc@users.noreply.github.com>
2020-08-18 16:55:07 +0700

    add package

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .js('resources/js/style.js', 'public/js')
+    .sass('resources/sass/style.scss', 'public/css')
+    .sass('resources/sass/app.scss', 'public/css')
+    .copy('node_modules/slick-carousel/slick/slick.min.js', 'public/js')
+    .copy('node_modules/slick-carousel/slick/slick.css', 'public/css')
+    .copy('node_modules/slick-carousel/slick/slick-theme.css', 'public/css');
