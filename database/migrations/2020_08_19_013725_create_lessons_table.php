a1b8cd999cf1a3a4d8cdf3762f2f821c5d13ebff
hapo-chinhnc <68211429+hapo-chinhnc@users.noreply.github.com>
2020-08-19 09:35:17 +0700

    add table to database

---
+++ database/migrations/2020_08_19_013725_create_lessons_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLessonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('descriptions')->nullable();
+            $table->text('requirements')->nullable();
+            $table->float('time');
+            $table->integer('course_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+}
