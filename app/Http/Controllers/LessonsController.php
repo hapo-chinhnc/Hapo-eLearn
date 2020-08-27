ba360b576943da66a31e862799837c9b4e3d7a7e
hapo-chinhnc <68211429+hapo-chinhnc@users.noreply.github.com>
2020-08-27 14:52:02 +0700

    View detail lesson

---
+++ app/Http/Controllers/LessonsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Lesson;
+use App\Models\Course;
+
+class LessonsController extends Controller
+{
+    public function show($id)
+    {
+        $lesson = Lesson::findOrFail($id);
+        $otherCourses = Course::inRandomOrder()->limit(config('variable.otherCourse'))->get();
+        return view('pages.lesson_detail', compact(['lesson', 'otherCourses']));
+    }
+}
