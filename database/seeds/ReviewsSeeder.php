220bae0fe17261624688caf74e314ce294f44163
hapo-chinhnc <68211429+hapo-chinhnc@users.noreply.github.com>
2020-08-31 13:31:19 +0700

    add reviews seeder

---
+++ database/seeds/ReviewsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Review;
+
+class ReviewsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Review::class, 50)->create();
+    }
+}
